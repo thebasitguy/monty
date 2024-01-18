4fd0e7cb71c45c97b18807d8b099af0dfe3729f6
thebasitguy <mailbasitgh@gmail.com>
2024-01-18 07:56:58 -0800

    stack_nop.c file

---
+++ stack_nop.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+
+/**
+ * stack_nop - performs a no-operation (nop) for a
+ * stack-based interpreter
+ *
+ * @head: stack head
+ * @counter: line_number
+ *
+ * Return: nothing
+ */
+
+void stack_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
